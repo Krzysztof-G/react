c3f388e6a30e11f64bb2bbddb118c90f1f59dd9d
Krzysztof Gumuła <k.gumula@icloud.com>
2020-04-27 21:52:55 +0200

    add columns

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-class Hero extends React.Component {
-  render() {
-    return (
+const Hero = props => (
         <header className={styles.component}>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img className={styles.image} src={props.imageText} />
+      </header>
+    )
+
+export default Hero;
+Hero.popTypes = {
+    titleText: PropTypes.node.isRequired,
+};
+
+// funkcja strzałkowa czemu wyswietla blad przy znaku = ?
+
+/* class Hero = () => (
+    <header className={styles.component}>
         <h2 className={styles.title}>Things to do</h2>
         <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
       </header>
-    )
-  }
-}
-
-export default Hero;
+) */
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js'
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js'
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+    }
+    static defaultProps = {
+        children: <p>I can do all Things!!!</p>
+    }
   render() {
     return (
       <section className={styles.component}>
-        <h2><Hero /></h2>
+        <h2><Hero titleText={this.props.title} imageText={this.props.image} /></h2>
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column columnTitle={'Animals'} />
+          <Column columnTitle={'Plants'} />
+          <Column columnTitle={'Minerals'} />
+        </div>
       </section>
     )
   }
