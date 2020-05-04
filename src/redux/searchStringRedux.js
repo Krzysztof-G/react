cbce4d4b2c3ed0ae981397c4d0c5cc52f65c0b37
Krzysztof Gumuła <k.gumula@icloud.com>
2020-05-04 19:35:38 +0200

    improve search functionality

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,16 +1,22 @@
 // selectors
 export const getSearchString = ({searchString}) => searchString;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 export const countAllCards = ({cards}) => cards.length;
-export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
-// action name creator
 
+// action name creator
+const reducerName = 'selectedCards';
+const createActionName = name => `app/${reducerName}/${name}`;
 // actions types
+export const CHANGE_CARD = createActionName('CHANGE_CARD');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload, type: CHANGE_CARD });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE_CARD:
+      return action.payload;
     default:
       return statePart;
   }
